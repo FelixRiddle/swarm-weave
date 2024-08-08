b6e73019897c278676f8ffd6739ecd83245a7f8e
Felix <fedecraft3312@gmail.com>
2024-08-07 23:05:58 -0300

    Added port option

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -16,7 +16,10 @@ struct Cli {
 #[derive(Subcommand)]
 enum Command {
     // Start the server
-    Server,
+    Server {
+        #[clap(short, long)]
+        port: Option<u16>,
+    },
     // Start the client
     Client,
 }
@@ -28,8 +31,12 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     
     match cli.command {
-        Command::Server => {
-            let options = StartServerOptions::default_controlled()?;
+        Command::Server { port } => {
+            // Create options
+            let mut options = StartServerOptions::default_controlled()?;
+            
+            options.port = port.unwrap_or(options.port);
+            
             if let Err(e) = server::start_server(options).await {
                 eprintln!("Error starting server: {}", e);
             } else {
