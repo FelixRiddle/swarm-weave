1c26788be5cc3da0a81d97e0b70d2dddb1ba8877
Felix <fedecraft3312@gmail.com>
2024-08-16 19:53:30 -0300

    Some fixes

--- src/server_node/mod.rs
+++ src/server_node/mod.rs
@@ -1,3 +1,4 @@
+use names::{Generator, Name};
 use get_if_addrs::{get_if_addrs, IfAddr};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
@@ -11,22 +12,58 @@ pub use resources::Resources;
 use crate::config::env::server_port;
 
 #[derive(Deserialize, Serialize)]
-pub enum Location {
-    IpAddress(IpAddress),
-    DomainName(String),
-}
-
-#[derive(Deserialize, Serialize)]
 pub struct IpAddress {
     pub address: String,
     pub port: u16,
 }
 
+impl IpAddress {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            address: get_computer_ip()?,
+            port: server_port().parse::<u16>()?,
+        })
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum ServerLocation {
+    IpAddress(IpAddress),
+    DomainName(String),
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ServerInfo {
+    // Display name
+    pub name: String,
+    pub hostname: Option<String>,
+    pub location: ServerLocation,
+}
+
+impl ServerInfo {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        let mut generator = Generator::with_naming(Name::Numbered);
+        let name = match generator.next() {
+            Some(name) => name,
+            None => return Err("Failed to generate a unique name for the server".into()),
+        };
+        
+        let hostname = None;
+        let ip_address = IpAddress::new()?;
+        let location = ServerLocation::IpAddress(ip_address);
+        
+        Ok(Self {
+            name,
+            hostname,
+            location,
+        })
+    }
+}
+
 #[derive(Deserialize, Serialize)]
 pub struct ServerNode {
     pub id: u32,
-    pub hostname: Option<String>,
-    pub location: Location,
+    pub location: ServerInfo,
     pub status: ServerStatus,
     pub resources: Resources,
     pub system_info: SystemInfo,
@@ -77,11 +114,7 @@ impl ServerNode {
     pub fn new(id: u32) -> Result<Self, Box<dyn Error>> {
         Ok(Self {
             id,
-            hostname: None,
-            location: Location::IpAddress(IpAddress {
-                address: get_computer_ip()?,
-                port: server_port().parse::<u16>()?,
-            }),
+            location: ServerInfo::new()?,
             status: ServerStatus::Online,
             resources: Resources::fetch_resources()?,
             system_info: SystemInfo::new(),
@@ -92,22 +125,42 @@ impl ServerNode {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
+    #[test]
+    fn test_server_info_new() {
+        let server_info = ServerInfo::new().unwrap();
+        assert!(server_info.name.len() > 0);
+        assert!(server_info.hostname.is_none());
+        match server_info.location {
+            ServerLocation::IpAddress(ip_address) => {
+                assert!(ip_address.address.len() > 0);
+                assert!(ip_address.port > 0);
+            }
+            ServerLocation::DomainName(_) => panic!("Unexpected DomainName"),
+        }
+    }
+
     #[test]
     fn test_server_node_new() {
-        let node = ServerNode::new(1).unwrap();
-        
-        assert_eq!(node.id, 1);
-        assert_eq!(node.status, ServerStatus::Online);
-        assert!(node.resources.cpus.len() > 0);
-        assert!(node.system_info.name.len() > 0);
+        let server_node = ServerNode::new(1).unwrap();
+        assert!(server_node.id > 0);
+        assert!(server_node.location.name.len() > 0);
+        assert_eq!(server_node.status, ServerStatus::Online);
+        assert!(server_node.system_info.name.len() > 0);
     }
-    
+
     #[test]
-    fn test_server_node_hostname() {
-        let mut node = ServerNode::new(1).unwrap();
-        node.hostname = Some("example.com".to_string());
-        
-        assert_eq!(node.hostname.unwrap(), "example.com");
+    fn test_system_info_new() {
+        let system_info = SystemInfo::new();
+        assert!(system_info.name.len() > 0);
+        assert!(system_info.kernel_version.len() > 0);
+        assert!(system_info.os_version.len() > 0);
+        assert!(system_info.host_name.len() > 0);
+    }
+
+    #[test]
+    fn test_get_computer_ip() {
+        let ip = get_computer_ip().unwrap();
+        assert!(ip.len() > 0);
     }
 }
