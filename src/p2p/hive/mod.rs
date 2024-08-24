9f7e9c9735c262300356d8107600a60a28a6b5a9
Felix <fedecraft3312@gmail.com>
2024-08-24 11:03:40 -0300

    Doing the relay connection

--- src/p2p/hive/mod.rs
+++ src/p2p/hive/mod.rs
@@ -11,16 +11,24 @@ pub mod server;
 
 #[derive(Parser)]
 pub struct HiveParameters {
+    /// Whether the applications acts as a client or server
     #[clap(short, long)]
     pub server: bool,
+    /// Server port
     #[clap(short, long)]
     pub port: Option<u16>,
+    /// Server address
     #[clap(long)]
     pub server_address: Option<Multiaddr>,
+    /// Remote server peer id
     #[clap(long)]
     pub server_peer_id: Option<PeerId>,
+    /// Key seed to generate keypair
     #[clap(long)]
     pub key_seed: Option<u8>,
+    /// Whether to use IPV6 or IPV4
+    #[clap(long)]
+    pub use_ipv6: Option<bool>,
 }
 
 /// Main function
