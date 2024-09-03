b3cfdc707705ad865c6b098eab7b792c2b842458
Felix <fedecraft3312@gmail.com>
2024-09-03 09:50:01 -0300

    System info, update doesn't seems to work here either

--- src/server_node/server_info.rs
+++ src/server_node/server_info.rs
@@ -2,7 +2,14 @@ use names::{Generator, Name};
 use get_if_addrs::{get_if_addrs, IfAddr};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
+use entity::server_location::{
+    Entity as ServerLocationEntity,
+    ActiveModel as ServerLocationActiveModel,
+    Model as ServerLocationModel,
+};
+use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection, EntityTrait};
 
+use crate::database::mysql_connection;
 use crate::config::env::server_port;
 
 /// Get computer IP v4
@@ -71,6 +78,94 @@ impl ServerInfo {
     }
 }
 
+impl Into<ServerInfo> for ServerLocationModel {
+    fn into(self) -> ServerInfo {
+        ServerInfo {
+            name: self.name,
+            hostname: self.domain.clone(),
+            location: match self.domain {
+                Some(domain_name) => ServerLocation::DomainName(domain_name),
+                None => match (self.address, self.port) {
+                    (Some(address), Some(port)) => ServerLocation::IpAddress(IpAddress {
+                        address,
+                        port: port as u16,
+                    }),
+                    _ => panic!("Invalid server location"),
+                },
+            },
+        }
+    }
+}
+
+impl ServerInfo {
+    pub fn into_active_model(&self) -> ServerLocationActiveModel {
+        ServerLocationActiveModel {
+            name: ActiveValue::Set(self.name.clone()),
+            domain: ActiveValue::Set(self.hostname.clone()),
+            address: ActiveValue::Set(match &self.location {
+                ServerLocation::IpAddress(ip_address) => Some(ip_address.address.clone()),
+                ServerLocation::DomainName(domain_name) => Some(domain_name.clone()),
+            }),
+            port: ActiveValue::Set(match &self.location {
+                ServerLocation::IpAddress(ip_address) => Some(ip_address.port as i32),
+                ServerLocation::DomainName(_) => None,
+            }),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ServerInfoController {
+    pub db: DatabaseConnection,
+    pub server_info: ServerInfo,
+}
+
+impl ServerInfoController {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let db = mysql_connection().await?;
+        let server_info = ServerInfo::new()?;
+        
+        Ok(Self { db, server_info })
+    }
+    
+    pub async fn insert(self) -> Result<ServerLocationModel, Box<dyn Error>> {
+        let result = self.server_info.clone()
+			.into_active_model()
+			.insert(&self.db)
+			.await?;
+        
+        Ok(result)
+    }
+    
+    pub async fn update(self) -> Result<Self, Box<dyn Error>> {
+        self.server_info.clone()
+			.into_active_model()
+			.update(&self.db)
+			.await?;
+        
+        Ok(self)
+    }
+    
+    pub async fn find(&mut self, id: i64) -> Result<&mut Self, Box<dyn Error>> {
+        let found_server_info: Option<ServerLocationModel> = ServerLocationEntity::find_by_id(id).one(&self.db).await?;
+        let server_info: ServerInfo = match found_server_info {
+            Some(model) => model.into(),
+            None => return Err("Server info not found".into()),
+        };
+        
+        self.server_info = server_info;
+        Ok(self)
+    }
+    
+    pub async fn delete(self, id: i64) -> Result<Self, Box<dyn Error>> {
+        let delete_result = ServerLocationEntity::delete_by_id(id).exec(&self.db).await?;
+        assert_eq!(delete_result.rows_affected, 1);
+        
+        Ok(self)
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
 	use super::*;
@@ -94,4 +189,50 @@ pub mod tests {
         let ip = get_computer_ip().unwrap();
         assert!(ip.len() > 0);
     }
+	
+	#[tokio::test]
+	async fn test_server_info_controller_new() {
+		let controller = ServerInfoController::new().await.unwrap();
+		assert!(controller.server_info.name.len() > 0);
+	}
+
+	#[tokio::test]
+	async fn test_server_info_controller_insert() {
+		let controller = ServerInfoController::new().await.unwrap();
+		
+		let inserted_model = controller.insert().await.unwrap();
+		assert!(inserted_model.id > 0);
+	}
+
+	#[tokio::test]
+	async fn test_server_info_controller_update() {
+		let mut controller = ServerInfoController::new().await.unwrap();
+		
+		controller.clone().insert().await.unwrap();
+		
+		controller.server_info.name = "Updated Name".to_string();
+		let updated_controller = controller.update().await.unwrap();
+		
+		assert_eq!(updated_controller.server_info.name, "Updated Name");
+	}
+	
+	#[tokio::test]
+	async fn test_server_info_controller_find() {
+		let mut controller = ServerInfoController::new().await.unwrap();
+		
+		let inserted_model = controller.clone().insert().await.unwrap();
+		
+		let found_controller = controller.find(inserted_model.id).await.unwrap();
+		assert_eq!(found_controller.server_info.name, inserted_model.name);
+	}
+	
+	#[tokio::test]
+	async fn test_server_info_controller_delete() {
+		let controller = ServerInfoController::new().await.unwrap();
+		
+		let inserted_model = controller.clone().insert().await.unwrap();
+		
+		let deleted_controller = controller.delete(inserted_model.id).await.unwrap();
+		assert_eq!(deleted_controller.server_info.name, inserted_model.name);
+	}
 }
