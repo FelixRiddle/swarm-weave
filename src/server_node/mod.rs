f888c1524123474afc41e1cf96012528c8b919a4
Felix <fedecraft3312@gmail.com>
2024-09-08 09:54:02 -0300

    Added function find by server node model

--- src/server_node/mod.rs
+++ src/server_node/mod.rs
@@ -1,91 +1,89 @@
-use sea_orm::{
-	ActiveValue,
-	DatabaseConnection,
-	EntityTrait,
-};
-use serde::{Deserialize, Serialize};
-use std::error::Error;
-use entity::server_node::{
-	Entity as ServerNodeEntity,
-	ActiveModel as ServerNodeActiveModel,
-};
+use entity::server_node::{ActiveModel as ServerNodeActiveModel, Entity as ServerNodeEntity};
 use entity::{
 	sea_orm_active_enums::Status,
-	server_location::ActiveModel as ServerLocationActiveModel,
-	system_resources::ActiveModel as SystemResourcesActiveModel,
+	server_location::{
+		ActiveModel as ServerLocationActiveModel,
+		Entity as ServerLocationEntity,
+	},
 	system_info::ActiveModel as SystemInfoActiveModel,
+	system_resources::ActiveModel as SystemResourcesActiveModel,
 };
+use sea_orm::{ActiveValue, DatabaseConnection, EntityTrait, IntoActiveModel};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 use strum_macros::Display;
 
-pub mod server_info;
-pub mod system_info;
 pub mod resources;
+pub mod server_info;
 pub mod storage;
+pub mod system_info;
 
 pub use resources::Resources;
+pub use server_info::{
+	ServerInfo,
+	ServerInfoController,
+};
 pub use system_info::SystemInfo;
-pub use server_info::ServerInfo;
 
-#[derive(Clone, Debug, Display, PartialEq)]
-#[derive(Deserialize, Serialize)]
+#[derive(Clone, Debug, Display, PartialEq, Deserialize, Serialize)]
 pub enum ServerStatus {
-    Online,
-    Offline,
-    Maintenance,
+	Online,
+	Offline,
+	Maintenance,
 }
 
 impl From<ServerStatus> for Status {
-    fn from(status: ServerStatus) -> Self {
-        match status {
-            ServerStatus::Online => Status::Online,
-            ServerStatus::Offline => Status::Offline,
-            ServerStatus::Maintenance => Status::Maintenance,
-        }
-    }
+	fn from(status: ServerStatus) -> Self {
+		match status {
+			ServerStatus::Online => Status::Online,
+			ServerStatus::Offline => Status::Offline,
+			ServerStatus::Maintenance => Status::Maintenance,
+		}
+	}
 }
 
 #[derive(Clone, Deserialize, Serialize)]
 pub struct ServerNode {
-    pub id: u32,
-    pub location: ServerInfo,
-    pub status: ServerStatus,
-    pub resources: Resources,
-    pub system_info: SystemInfo,
+	pub id: u32,
+	pub location: ServerInfo,
+	pub status: ServerStatus,
+	pub resources: Resources,
+	pub system_info: SystemInfo,
 }
 
 impl ServerNode {
-    pub fn new(id: u32) -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            id,
-            location: ServerInfo::new()?,
-            status: ServerStatus::Online,
-            resources: Resources::fetch_resources()?,
-            system_info: SystemInfo::new(),
-        })
-    }
-	
+	pub fn new(id: u32) -> Result<Self, Box<dyn Error>> {
+		Ok(Self {
+			id,
+			location: ServerInfo::new()?,
+			status: ServerStatus::Online,
+			resources: Resources::fetch_resources()?,
+			system_info: SystemInfo::new(),
+		})
+	}
+
 	/// Try into active model
-	/// 
-	/// 
-    pub fn try_into_active_model(
+	///
+	///
+	pub fn try_into_active_model(
 		self,
 		server_location_id: i64,
 		resource_id: i64,
-		system_info_id: i64
+		system_info_id: i64,
 	) -> Result<ServerNodeActiveModel, Box<dyn Error>> {
-        Ok(ServerNodeActiveModel {
-            id: ActiveValue::Set(i64::try_from(self.id)?),
-            status: ActiveValue::Set(Some(self.status.into())),
+		Ok(ServerNodeActiveModel {
+			id: ActiveValue::Set(i64::try_from(self.id)?),
+			status: ActiveValue::Set(Some(self.status.into())),
 			server_location_id: ActiveValue::Set(Some(server_location_id)),
 			system_resource_id: ActiveValue::Set(Some(resource_id)),
-            system_info_id: ActiveValue::Set(Some(system_info_id)),
+			system_info_id: ActiveValue::Set(Some(system_info_id)),
 			..Default::default()
-        })
-    }
+		})
+	}
 }
 
 /// Server node controller
-/// 
+///
 /// Mainly for database manipulation
 pub struct ServerNodeController {
 	pub db: DatabaseConnection,
@@ -99,156 +97,165 @@ pub struct ServerNodeController {
 
 impl ServerNodeController {
 	/// Create new
-	/// 
+	///
 	/// Fetch resources on creation
 	pub async fn new(
 		db: DatabaseConnection,
 		server_node_active_model: Option<ServerNodeActiveModel>,
 		server_location: ServerLocationActiveModel,
 		system_resources: SystemResourcesActiveModel,
-		system_info: SystemInfoActiveModel
+		system_info: SystemInfoActiveModel,
 	) -> Result<Self, Box<dyn Error>> {
 		let server_node = ServerNode::new(1)?;
-		
+
 		Ok(Self {
 			db,
 			server_node,
 			server_node_active_model,
 			server_location,
 			system_resources,
-			system_info
+			system_info,
 		})
 	}
-	
+
 	// /// Create new from server node id
 	// /// 
-	// /// TODO: This
-	// pub async fn new_from_server_node(id: u64) -> Result<Self, Box<dyn Error>> {
-		
+	// /// 
+	// pub async fn new_from_server_node(db: DatabaseConnection, id: u32) -> Result<Self, Box<dyn Error>> {
+	// 	let server_node_active_model = ServerNodeEntity::find_by_id(id).one(&db).await?;
+	// 	match server_node_active_model {
+	// 		Some(server_node_active_model) => {
+	// 			// Find server location
+	// 			let server_location_model = ServerInfoController::find_by_server_node_model(db.clone(), server_node_active_model)
+	// 				.await?;
+	// 			let server_location = match ServerInfo::from_model(server_location_model.clone()) {
+	// 				Some(server_location) => server_location,
+	// 				None => return Err("Couldn't convert server location model to server info".into()),
+	// 			};
+				
+	// 			let server_node = ServerNode {
+	// 				id: server_node_active_model.id.try_into()?,
+	// 				location: server_location,
+	// 				status: server_node_active_model.status.into(),
+	// 				resources: Resources::from_active_model(system_resources.ok_or("System resources not found")?)?,
+	// 				system_info: SystemInfo::from_active_model(system_info.ok_or("System info not found")?)?,
+	// 			};
+	// 			Ok(Self {
+	// 				db,
+	// 				server_node,
+	// 				server_node_active_model: Some(server_node_active_model),
+	// 				server_location: server_location_model.into_active_model(),
+	// 				system_resources: system_resources.ok_or("System resources not found")?,
+	// 				system_info: system_info.ok_or("System info not found")?,
+	// 			})
+	// 		},
+	// 		None => Err("Server node not found".into()),
+	// 	}
 	// }
-	
+
 	/// Get or create server node active model
-	/// 
+	///
 	/// On creation the server node will be inserted, to make things faster
-	pub async fn get_server_node_active_model(&mut self) -> Result<ServerNodeActiveModel, Box<dyn Error>> {
+	pub async fn get_server_node_active_model(
+		&mut self,
+	) -> Result<ServerNodeActiveModel, Box<dyn Error>> {
 		let model = match &self.server_node_active_model {
 			Some(model) => model.clone(),
-            None => {
-                let server_location_id = match self.server_location.id.clone().take() {
+			None => {
+				let server_location_id = match self.server_location.id.clone().take() {
 					Some(id) => id,
-                    None => return Err("Server location id is not provided".into()),
+					None => return Err("Server location id is not provided".into()),
 				};
-                let system_resource_id = match self.system_resources.id.clone().take() {
+				let system_resource_id = match self.system_resources.id.clone().take() {
 					Some(id) => id,
-                    None => return Err("System resource id is not provided".into()),
+					None => return Err("System resource id is not provided".into()),
 				};
-                let system_info_id = match self.system_info.id.clone().take() {
+				let system_info_id = match self.system_info.id.clone().take() {
 					Some(id) => id,
-                    None => return Err("System info id is not provided".into()),
+					None => return Err("System info id is not provided".into()),
 				};
-                let model = self.server_node.clone().try_into_active_model(
-                    server_location_id,
-                    system_resource_id,
-                    system_info_id
-                )?;
-				
-                self.server_node_active_model = Some(model.clone());
-				
-                model
-            }
+				let model = self.server_node.clone().try_into_active_model(
+					server_location_id,
+					system_resource_id,
+					system_info_id,
+				)?;
+
+				self.server_node_active_model = Some(model.clone());
+
+				model
+			}
 		};
-		
+
 		Ok(model)
 	}
-    
-    /// Insert
-    /// 
-    /// 
-    pub async fn insert(&mut self) -> Result<&mut Self, Box<dyn Error>> {
+
+	/// Insert
+	///
+	///
+	pub async fn insert(&mut self) -> Result<&mut Self, Box<dyn Error>> {
 		self.get_server_node_active_model().await?;
-        Ok(self)
-    }
-    
-    // /// Update
-    // /// 
-    // /// 
-    // pub async fn update(self) -> Result<Self, Box<dyn Error>> {
-    //     let update_result = self.server_node.clone().into_active_model().update(&self.db).await?;
-        
-    //     Ok(self)
-    // }
-    
-    // /// Find
-    // /// 
-    // /// 
-    // pub async fn find(self, id: u32) -> Result<Self, Box<dyn Error>> {
-    //     let found_server_node = ServerNodeEntity::find_by_id(id).one(&self.db).await?;
-    //     match found_server_node {
-    //         Some(server_node) => {
-    //             let server_node = server_node.try_into_model()?;
-    //             Ok(Self { db: self.db, server_node })
-    //         },
-    //         None => Err("Server node not found".into()),
-    //     }
-    // }
-    
-    /// Delete
-    /// 
-    /// 
-    pub async fn delete(&mut self) -> Result<&mut Self, Box<dyn Error>> {
+		Ok(self)
+	}
+
+	/// Delete
+	///
+	///
+	pub async fn delete(&mut self) -> Result<&mut Self, Box<dyn Error>> {
 		let server_node_active_model = self.get_server_node_active_model().await?;
 		let id = match server_node_active_model.id.try_as_ref() {
 			Some(id) => id,
 			None => return Err("Server node id doesn't exists".into()),
 		};
-        ServerNodeEntity::delete_by_id(id.clone()).exec(&self.db).await?;
-        
-        Ok(self)
-    }
-	
-    /// Delete by id
-    /// 
-    /// 
-    pub async fn delete_by_id(db: &DatabaseConnection, id: u32) -> Result<(), Box<dyn Error>> {
-        ServerNodeEntity::delete_by_id(id).exec(db).await?;
-        
-        Ok(())
-    }
+		ServerNodeEntity::delete_by_id(id.clone())
+			.exec(&self.db)
+			.await?;
+
+		Ok(self)
+	}
+
+	/// Delete by id
+	///
+	///
+	pub async fn delete_by_id(db: &DatabaseConnection, id: u32) -> Result<(), Box<dyn Error>> {
+		ServerNodeEntity::delete_by_id(id).exec(db).await?;
+
+		Ok(())
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_server_node_new() {
-        let server_node = ServerNode::new(1).unwrap();
-        assert!(server_node.id > 0);
-        assert!(server_node.location.name.len() > 0);
-        assert_eq!(server_node.status, ServerStatus::Online);
-        assert!(server_node.system_info.name.len() > 0);
-    }
-	
+	#[test]
+	fn test_server_node_new() {
+		let server_node = ServerNode::new(1).unwrap();
+		assert!(server_node.id > 0);
+		assert!(server_node.location.name.len() > 0);
+		assert_eq!(server_node.status, ServerStatus::Online);
+		assert!(server_node.system_info.name.len() > 0);
+	}
+
 	/// Test serialization
-	/// 
-	/// 
+	///
+	///
 	#[test]
 	fn test_server_status_serialization() {
 		let status = ServerStatus::Online;
 		let serialized_status = serde_json::to_string(&status).unwrap();
-		
+
 		// It looks like this, not what I expected
 		assert_eq!("\"Online\"", serialized_status);
 	}
-	
+
 	/// Test to string
-	/// 
-	/// 
+	///
+	///
 	#[test]
 	fn test_server_status_to_string() {
 		let status = ServerStatus::Online;
 		let serialized_status = status.to_string();
-		
+
 		assert_eq!("Online", serialized_status);
 	}
 }
--- src/server_node/server_info.rs
+++ src/server_node/server_info.rs
@@ -1,10 +1,13 @@
-use entity::server_location::{
-    ActiveModel as ServerLocationActiveModel, Entity as ServerLocationEntity,
-    Model as ServerLocationModel,
+use entity::{
+	server_location::{
+		ActiveModel as ServerLocationActiveModel, Entity as ServerLocationEntity,
+		Model as ServerLocationModel,
+	},
+	server_node::Model as ServerNodeModel,
 };
 use get_if_addrs::{get_if_addrs, IfAddr};
 use names::{Generator, Name};
-use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection, EntityTrait};
+use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection, EntityTrait, IntoActiveModel};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 
@@ -15,69 +18,79 @@ use crate::database::mysql_connection;
 ///
 ///
 pub fn get_computer_ip() -> Result<String, Box<dyn Error>> {
-    let interfaces = get_if_addrs()?;
-    for interface in interfaces {
-        if let IfAddr::V4(addr) = interface.addr {
-            let ip = addr.ip;
-            if !ip.is_loopback() {
-                return Ok(ip.to_string());
-            }
-        }
-    }
+	let interfaces = get_if_addrs()?;
+	for interface in interfaces {
+		if let IfAddr::V4(addr) = interface.addr {
+			let ip = addr.ip;
+			if !ip.is_loopback() {
+				return Ok(ip.to_string());
+			}
+		}
+	}
 
-    Ok("0.0.0.0".to_string())
+	Ok("0.0.0.0".to_string())
 }
 
 #[derive(Clone, Deserialize, Serialize)]
 pub struct IpAddress {
-    pub address: String,
-    pub port: u16,
+	pub address: String,
+	pub port: u16,
 }
 
 impl IpAddress {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            address: get_computer_ip()?,
-            port: server_port().parse::<u16>()?,
-        })
-    }
+	pub fn new() -> Result<Self, Box<dyn Error>> {
+		Ok(Self {
+			address: get_computer_ip()?,
+			port: server_port().parse::<u16>()?,
+		})
+	}
 }
 
 #[derive(Clone, Deserialize, Serialize)]
 pub enum ServerLocation {
-    IpAddress(IpAddress),
-    DomainName(String),
+	IpAddress(IpAddress),
+	DomainName(String),
 }
 
 #[derive(Clone, Deserialize, Serialize)]
 pub struct ServerInfo {
-    // Display name
-    pub name: String,
-    pub hostname: Option<String>,
-    pub location: ServerLocation,
+	// Display name
+	pub name: String,
+	pub hostname: Option<String>,
+	pub location: ServerLocation,
 	// TODO: Location can be any of the two
 	//pub domain: Option<String>,
 	//pub address: Option<IpAddress>,
 }
 
 impl ServerInfo {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
-        let mut generator = Generator::with_naming(Name::Numbered);
-        let name = match generator.next() {
-            Some(name) => name,
-            None => return Err("Failed to generate a unique name for the server".into()),
-        };
+	pub fn new() -> Result<Self, Box<dyn Error>> {
+		let mut generator = Generator::with_naming(Name::Numbered);
+		let name = match generator.next() {
+			Some(name) => name,
+			None => return Err("Failed to generate a unique name for the server".into()),
+		};
 
-        let hostname = None;
-        let ip_address = IpAddress::new()?;
-        let location = ServerLocation::IpAddress(ip_address);
+		let hostname = None;
+		let ip_address = IpAddress::new()?;
+		let location = ServerLocation::IpAddress(ip_address);
 
-        Ok(Self {
-            name,
-            hostname,
-            location,
-        })
-    }
+		Ok(Self {
+			name,
+			hostname,
+			location,
+		})
+	}
+	
+	/// Create from model
+	/// 
+	/// 
+	pub fn from_model(model: ServerLocationModel) -> Option<Self> {
+		let model = model.into_active_model();
+		let model = Self::from_active_model(model);
+		
+		model
+	}
 	
 	/// Create from active model
 	/// 
@@ -99,7 +112,7 @@ impl ServerInfo {
 			(Some(domain_name), _, _) => {
 				match domain_name {
 					Some(domain_name) => Some(ServerLocation::DomainName(domain_name)),
-                    None => None
+					None => None
 				}
 			},
 			(_, Some(address), Some(port)) => Some(ServerLocation::IpAddress(IpAddress {
@@ -111,7 +124,7 @@ impl ServerInfo {
 		
 		let location = match location {
 			Some(location) => location,
-            None => return None,
+			None => return None,
 		};
 		
 		Some(Self {
@@ -123,67 +136,73 @@ impl ServerInfo {
 }
 
 impl Into<ServerInfo> for ServerLocationModel {
-    fn into(self) -> ServerInfo {
-        ServerInfo {
-            name: self.name,
-            hostname: self.domain.clone(),
-            location: match self.domain {
-                Some(domain_name) => ServerLocation::DomainName(domain_name),
-                None => match (self.address, self.port) {
-                    (Some(address), Some(port)) => ServerLocation::IpAddress(IpAddress {
-                        address,
-                        port: port as u16,
-                    }),
-                    _ => panic!("Invalid server location"),
-                },
-            },
-        }
-    }
+	fn into(self) -> ServerInfo {
+		ServerInfo {
+			name: self.name,
+			hostname: self.domain.clone(),
+			location: match self.domain {
+				Some(domain_name) => ServerLocation::DomainName(domain_name),
+				None => match (self.address, self.port) {
+					(Some(address), Some(port)) => ServerLocation::IpAddress(IpAddress {
+						address,
+						port: port as u16,
+					}),
+					_ => panic!("Invalid server location"),
+				},
+			},
+		}
+	}
 }
 
+/// Conversions
+/// 
+/// 
 impl ServerInfo {
-    pub fn into_active_model(&self) -> ServerLocationActiveModel {
-        ServerLocationActiveModel {
-            name: ActiveValue::Set(self.name.clone()),
-            domain: ActiveValue::Set(self.hostname.clone()),
-            address: ActiveValue::Set(match &self.location {
-                ServerLocation::IpAddress(ip_address) => Some(ip_address.address.clone()),
-                ServerLocation::DomainName(domain_name) => Some(domain_name.clone()),
-            }),
-            port: ActiveValue::Set(match &self.location {
-                ServerLocation::IpAddress(ip_address) => Some(ip_address.port as i32),
-                ServerLocation::DomainName(_) => None,
-            }),
-            ..Default::default()
-        }
-    }
+	/// Convert into active model
+	/// 
+	/// 
+	pub fn into_active_model(&self) -> ServerLocationActiveModel {
+		ServerLocationActiveModel {
+			name: ActiveValue::Set(self.name.clone()),
+			domain: ActiveValue::Set(self.hostname.clone()),
+			address: ActiveValue::Set(match &self.location {
+				ServerLocation::IpAddress(ip_address) => Some(ip_address.address.clone()),
+				ServerLocation::DomainName(domain_name) => Some(domain_name.clone()),
+			}),
+			port: ActiveValue::Set(match &self.location {
+				ServerLocation::IpAddress(ip_address) => Some(ip_address.port as i32),
+				ServerLocation::DomainName(_) => None,
+			}),
+			..Default::default()
+		}
+	}
 }
 
 #[derive(Clone)]
 pub struct ServerInfoController {
-    pub db: DatabaseConnection,
-    pub server_info: ServerInfo,
+	pub db: DatabaseConnection,
+	pub server_info: ServerInfo,
 }
 
 impl ServerInfoController {
-    pub async fn new() -> Result<Self, Box<dyn Error>> {
-        let db = mysql_connection().await?;
-        let server_info = ServerInfo::new()?;
+	pub async fn new() -> Result<Self, Box<dyn Error>> {
+		let db = mysql_connection().await?;
+		let server_info = ServerInfo::new()?;
 
-        Ok(Self { db, server_info })
-    }
+		Ok(Self { db, server_info })
+	}
+	
+	pub async fn insert(self) -> Result<ServerLocationModel, Box<dyn Error>> {
+		let result = self
+			.server_info
+			.clone()
+			.into_active_model()
+			.insert(&self.db)
+			.await?;
 
-    pub async fn insert(self) -> Result<ServerLocationModel, Box<dyn Error>> {
-        let result = self
-            .server_info
-            .clone()
-            .into_active_model()
-            .insert(&self.db)
-            .await?;
-
-        Ok(result)
-    }
-
+		Ok(result)
+	}
+	
 	pub async fn update(self) -> Result<Self, Box<dyn Error>> {
 		let updated_active_model = self.server_info
 			.clone()
@@ -201,80 +220,102 @@ impl ServerInfoController {
 			server_info: updated_server_info,
 		})
 	}
+	
+	/// Find by id
+	/// 
+	/// 
+	pub async fn find(&mut self, id: i64) -> Result<&mut Self, Box<dyn Error>> {
+		let found_server_info: Option<ServerLocationModel> =
+			ServerLocationEntity::find_by_id(id).one(&self.db).await?;
+		let server_info: ServerInfo = match found_server_info {
+			Some(model) => model.into(),
+			None => return Err("Server info not found".into()),
+		};
 
-    pub async fn find(&mut self, id: i64) -> Result<&mut Self, Box<dyn Error>> {
-        let found_server_info: Option<ServerLocationModel> =
-            ServerLocationEntity::find_by_id(id).one(&self.db).await?;
-        let server_info: ServerInfo = match found_server_info {
-            Some(model) => model.into(),
-            None => return Err("Server info not found".into()),
-        };
+		self.server_info = server_info;
+		Ok(self)
+	}
+	
+	/// Find by server node model
+	/// 
+	/// 
+	pub async fn find_by_server_node_model(
+		db: DatabaseConnection,
+		server_node_active_model: ServerNodeModel
+	) -> Result<ServerLocationModel, Box<dyn Error>> {
+		let server_location_id = match server_node_active_model.server_location_id {
+			Some(id) => id,
+			None => return Err("Server location id not found".into()),
+		};
+		let server_location = match ServerLocationEntity::find_by_id(server_location_id).one(&db).await? {
+			Some(model) => model,
+            None => return Err("Server location not found".into()),
+		};
+		
+		Ok(server_location)
+	}
+	
+	pub async fn delete(self, id: i64) -> Result<Self, Box<dyn Error>> {
+		ServerLocationEntity::delete_by_id(id)
+			.exec(&self.db)
+			.await?;
 
-        self.server_info = server_info;
-        Ok(self)
-    }
-
-    pub async fn delete(self, id: i64) -> Result<Self, Box<dyn Error>> {
-        ServerLocationEntity::delete_by_id(id)
-            .exec(&self.db)
-            .await?;
-
-        Ok(self)
-    }
+		Ok(self)
+	}
 }
 
 #[cfg(test)]
 pub mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_server_info_new() {
-        let server_info = ServerInfo::new().unwrap();
-        assert!(server_info.name.len() > 0);
-        assert!(server_info.hostname.is_none());
-        match server_info.location {
-            ServerLocation::IpAddress(ip_address) => {
-                assert!(ip_address.address.len() > 0);
-                assert!(ip_address.port > 0);
-            }
-            ServerLocation::DomainName(_) => panic!("Unexpected DomainName"),
-        }
-    }
+	#[test]
+	fn test_server_info_new() {
+		let server_info = ServerInfo::new().unwrap();
+		assert!(server_info.name.len() > 0);
+		assert!(server_info.hostname.is_none());
+		match server_info.location {
+			ServerLocation::IpAddress(ip_address) => {
+				assert!(ip_address.address.len() > 0);
+				assert!(ip_address.port > 0);
+			}
+			ServerLocation::DomainName(_) => panic!("Unexpected DomainName"),
+		}
+	}
 
-    #[test]
-    fn test_get_computer_ip() {
-        let ip = get_computer_ip().unwrap();
-        assert!(ip.len() > 0);
-    }
+	#[test]
+	fn test_get_computer_ip() {
+		let ip = get_computer_ip().unwrap();
+		assert!(ip.len() > 0);
+	}
 
-    #[tokio::test]
-    async fn test_server_info_controller_new() {
-        let controller = ServerInfoController::new().await.unwrap();
-        assert!(controller.server_info.name.len() > 0);
-    }
+	#[tokio::test]
+	async fn test_server_info_controller_new() {
+		let controller = ServerInfoController::new().await.unwrap();
+		assert!(controller.server_info.name.len() > 0);
+	}
 
-    #[tokio::test]
-    async fn test_server_info_controller_insert() {
-        let controller = ServerInfoController::new().await.unwrap();
+	#[tokio::test]
+	async fn test_server_info_controller_insert() {
+		let controller = ServerInfoController::new().await.unwrap();
 
-        let inserted_model = controller.insert().await.unwrap();
-        assert!(inserted_model.id > 0);
-    }
+		let inserted_model = controller.insert().await.unwrap();
+		assert!(inserted_model.id > 0);
+	}
 
-    #[tokio::test]
-    async fn test_server_info_controller_update() {
-        let mut controller = ServerInfoController::new().await.unwrap();
+	#[tokio::test]
+	async fn test_server_info_controller_update() {
+		let mut controller = ServerInfoController::new().await.unwrap();
 
-        controller.clone().insert().await.unwrap();
+		controller.clone().insert().await.unwrap();
 
-        controller.server_info.name = "Updated Name".to_string();
-        let mut active_model = controller.server_info.clone().into_active_model();
-        active_model.name = ActiveValue::Set(controller.server_info.name.clone());
+		controller.server_info.name = "Updated Name".to_string();
+		let mut active_model = controller.server_info.clone().into_active_model();
+		active_model.name = ActiveValue::Set(controller.server_info.name.clone());
 
-        let mut updated_model = active_model.save(&controller.db).await.unwrap();
+		let mut updated_model = active_model.save(&controller.db).await.unwrap();
 
-        assert_eq!(updated_model.name.take().unwrap(), "Updated Name");
-    }
+		assert_eq!(updated_model.name.take().unwrap(), "Updated Name");
+	}
 	
 	#[tokio::test]
 	async fn test_server_info_controller_update_using_controller() {
@@ -289,23 +330,23 @@ pub mod tests {
 		assert_eq!(updated_controller.server_info.name, "Updated Name");
 	}
 	
-    #[tokio::test]
-    async fn test_server_info_controller_find() {
-        let mut controller = ServerInfoController::new().await.unwrap();
+	#[tokio::test]
+	async fn test_server_info_controller_find() {
+		let mut controller = ServerInfoController::new().await.unwrap();
 
-        let inserted_model = controller.clone().insert().await.unwrap();
+		let inserted_model = controller.clone().insert().await.unwrap();
 
-        let found_controller = controller.find(inserted_model.id).await.unwrap();
-        assert_eq!(found_controller.server_info.name, inserted_model.name);
-    }
+		let found_controller = controller.find(inserted_model.id).await.unwrap();
+		assert_eq!(found_controller.server_info.name, inserted_model.name);
+	}
 
-    #[tokio::test]
-    async fn test_server_info_controller_delete() {
-        let controller = ServerInfoController::new().await.unwrap();
+	#[tokio::test]
+	async fn test_server_info_controller_delete() {
+		let controller = ServerInfoController::new().await.unwrap();
 
-        let inserted_model = controller.clone().insert().await.unwrap();
+		let inserted_model = controller.clone().insert().await.unwrap();
 
-        let deleted_controller = controller.delete(inserted_model.id).await.unwrap();
-        assert_eq!(deleted_controller.server_info.name, inserted_model.name);
-    }
+		let deleted_controller = controller.delete(inserted_model.id).await.unwrap();
+		assert_eq!(deleted_controller.server_info.name, inserted_model.name);
+	}
 }
